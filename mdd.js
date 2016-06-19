57bb11f23f32511ab3513656dbdac8d19d85ce11
bitliner <giovanni.gaglione@gmail.com>
2016-06-19 13:52:13 +0000

    +added code to make scope extend TypeUtils

--- mdd.js
+++ mdd.js
@@ -6,14 +6,16 @@
  * @description
  * # mdd
  */
-angular.module('mdd',[])
+angular.module('mdd', [])
 	.directive('mdd', function() {
 		return {
 			templateUrl: 'views/mdd.html',
 			restrict: 'E',
-			link: function postLink(scope) {
+			link: function postLink(scope, TypeUtils) {
 
+				angular.extend(scope, TypeUtils);
 
+				console.log('ola')
 
 
 
