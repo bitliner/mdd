88eab85a6a5a1f43f40541ac69175823cfe28d15
bitliner <giovanni.gaglione@gmail.com>
2016-06-19 14:39:32 +0000

    +added multiple directives

--- mdd.js
+++ mdd.js
@@ -7,15 +7,72 @@
  * # mdd
  */
 angular.module('mdd', [])
-	.directive('mdd', function() {
+	.directive('mdd', function(TypeUtils) {
 		return {
+			scope:{
+				data:'='
+			},
 			templateUrl: 'views/mdd.html',
 			restrict: 'E',
-			link: function postLink(scope, TypeUtils) {
+			link: function postLink(scope) {
 
 				angular.extend(scope, TypeUtils);
 
 				console.log('ola')
+
+
+
+			}
+		};
+	})
+	.directive('mddArray', function(TypeUtils) {
+		return {
+			scope:{
+				data:'='
+			},
+			templateUrl: 'views/mdd-array.html',
+			restrict: 'E',
+			link: function postLink(scope) {
+
+				angular.extend(scope, TypeUtils);
+
+				console.log('Array')
+
+
+
+			}
+		};
+	})
+	.directive('mddObject', function(TypeUtils) {
+		return {
+			scope:{
+				data:'='
+			},
+			templateUrl: 'views/mdd-object.html',
+			restrict: 'E',
+			link: function postLink(scope) {
+
+				angular.extend(scope, TypeUtils);
+
+				console.log('Object')
+
+
+
+			}
+		};
+	})
+	.directive('mddSimpleValue', function(TypeUtils) {
+		return {
+			scope:{
+				data:'='
+			},
+			templateUrl: 'views/mdd-simple-value.html',
+			restrict: 'E',
+			link: function postLink(scope) {
+
+				angular.extend(scope, TypeUtils);
+
+				console.log('SimpleValue')
 
 
 
